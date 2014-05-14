970c1b6f32a0a75d4b7f533b2085ecea6bb58e6e
Lemonclowder <littlebenji702@hotmail.com>
2014-05-14 14:33:51 +0200

    Menu class implementerad

--- Lean/Menu.h
+++ Lean/Menu.h
@@ -1,16 +1,34 @@
-////////////////////////////////////////////////////////////////////////////////
-// Filename:  Menu.h
-////////////////////////////////////////////////////////////////////////////////
-
 #pragma once
+
+#ifndef MENU_H
+#define MENU_H
+
+#include "D3D.h"
+#include "Input.h"
+#include "Camera.h"
+#include "SentenceClass.h"
+#include "ImageClass.h"
+
 class Menu
 {
 public:
 	Menu();
 	~Menu();
-	bool Initialize();
-	void Update();
-	void Render();
+
+	bool Initialize(D3D* d3d, Input* inputPtr, Camera* cameraPtr, int screenWidth, int screenHeight, m4 baseViewMatrix);
+	bool Update(float deltaTime);
+	void Render(D3D* d3d);
 	void Shutdown();
+
+private:
+	ImageClass*		mLogo;
+	SentenceClass*	mOption1;
+	SentenceClass*	mOption2;
+	Input*			mInputPtr;
+	Camera*			mCameraPtr;
+
+	int mScreenWidth, mScreenHeight, decision, nrOfOptions;
+	float inputCooldown;
 };
 
+#endif
